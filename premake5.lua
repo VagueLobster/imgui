73abe8225d62889a50571c4815dc7d1389892d33
David Mortensen <davidm@nutcrackz.com>
2022-04-03 12:26:11 +0200

    Updated imgui to be able to use nodes
    - Node editor can be used with this version of imgui

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -13,6 +13,7 @@ project "ImGui"
 		"imgui.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
+		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
